8d7f60d5200512b94b58828f4aa5179ba1db64ae
yeturu surya <yeturusurya2@gmail.com>
2026-06-20 16:46:13 +0530

    fix render trust proxy

--- server/server.js
+++ server/server.js
@@ -46,6 +46,7 @@ const emailProvider = (() => {
 })();
 
 const app = express();
+app.set("trust proxy", 1);
 
 const allowedOrigins = (process.env.CLIENT_URL || 'http://localhost:5173')
   .split(',')
